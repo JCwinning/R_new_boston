681b4bc1a13b54c8c4df26ee817dd2e8fb4b2fd0
JC <verykoala@gmail.com>
2017-01-02 00:46:36 +0800

    R project update

--- TheNEW_R001.R
+++ TheNEW_R001.R
@@ -63,30 +63,38 @@
 # a
 # a[1]
 # R Programming Tutorial - 11 - Data Frame
-id <- 1:15
-age <- c(18,13,66,32,3,43,54,656,87,323,7,2,9,34,65)
-name <-c("bucky","tom","bobby","henry","emily","baby","hannah","joe","cathy","sandY","lesley","emma","ann","old dan","eric")
-x=data.frame(id,age,name)
-x
-dim(x)
-nrow(x)
-ncol(x)
-names(x)
-names(x)[1]
-head(x)
-tail(x)
-class(x)
-# R Programming Tutorial - 12 - More on Data Frames
-x[2]
-x$age
-x["age"]
-class(x$age)
-class(x["age"])
-class(x[2])
-class(x[,2])
-x[2,3]
-x[3,1]
-x[3,1:3]
-x[3,]
-x[,2]
-x[2]
+# id <- 1:15
+# age <- c(18,13,66,32,3,43,54,656,87,323,7,2,9,34,65)
+# name <-c("bucky","tom","bobby","henry","emily","baby","hannah","joe","cathy","sandY","lesley","emma","ann","old dan","eric")
+# x=data.frame(id,age,name)
+# x
+# dim(x)
+# nrow(x)
+# ncol(x)
+# names(x)
+# names(x)[1]
+# head(x)
+# tail(x)
+# class(x)
+# # R Programming Tutorial - 12 - More on Data Frames
+# x[2]
+# x$age
+# x["age"]
+# class(x$age)
+# class(x["age"])
+# class(x[2])
+# class(x[,2])
+# x[2,3]
+# x[3,1]
+# x[3,1:3]
+# x[3,]
+# x[,2]
+# x[2]
+
+# R Programming Tutorial - 13 - Lists
+Tony_list=list(71,"hello",x)
+class(Tony_list)  
+names(Tony_list)=c('num','word','thisisdata') 
+  
+  
+  
